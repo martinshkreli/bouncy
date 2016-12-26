4b7be09fc1dbb255ebc08948728d53a43177348e
Martin Shkreli <martin@turingpharma.com>
2016-12-26 17:40:19 -0500

    colors?
    Signed-off-by: Martin Shkreli <martin@turingpharma.com>

--- socket.js
+++ socket.js
@@ -2,6 +2,7 @@ var io = require('socket.io');
 var userCount = 0;
 var names = ['Martin', 'Mark', 'Sally', 'Lil Wayne', 'Eminem', 'Dre', 'Xzibit', 'DMX', 'Florida', 'Jinx', 'ODB']
 var users = [];
+var colors = ['red', 'blue', 'orange', 'yellow', 'green', 'purple', 'pink', 'brown', 'magenta', 'indigo'];
 var userProto = {
   name: '',
   radius: 0,
@@ -20,17 +21,21 @@ exports.initialize = function(server) {
     users[userCount] = Object.create(userProto);
     var rnd = Math.floor((Math.random() * 10) + 0);
     users[userCount].name = names[rnd];
+    users[userCount].color = colors[rnd];
+    console.log("Color is: ");
+    console.log(users[userCount].color);
     socket.send(JSON.stringify(
       {
         type: 'serverMessage',
         message: 'Welcome to Bouncy ' + users[userCount].name + '! ' + userCount,
-        userId: userCount
+        userId: userCount,
+        color: users[userCount].color
       }
     ));
     socket.on('message', function(message){
       //PARSE THE MESSAGE FROM STRING BACK TO JSON
       message = JSON.parse(message);
-      
+
       if (message.type == 'userAction') {
         console.log("got userAction message!");
         socket.broadcast.send(JSON.stringify(message));
